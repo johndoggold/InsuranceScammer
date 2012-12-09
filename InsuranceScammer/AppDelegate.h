95212131d4b447ce08254273ff8365210adf0ff1
John Francisco <johnfrancisco@John-Franciscos-MacBook-Pro.local>
2012-12-09 12:51:34 +0800

    Initial Commit

---
+++ InsuranceScammer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InsuranceScammer
+//
+//  Created by John Francisco on 9/12/12.
+//  Copyright (c) 2012 John Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
